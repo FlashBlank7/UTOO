37d1e8930813d2607fdb665a235f25d0bab5a17b
ZHONG Haoyang <aitanxiandeqimiao@gmail.com>
2026-06-24 14:55:18 +0900

    Configure Azure app service during deployment

--- backend/startup.sh
+++ backend/startup.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 alembic upgrade head
 
 echo "Starting Azure App Service web server..."
-exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 app.main:app
+exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b "0.0.0.0:${PORT:-8000}" app.main:app
